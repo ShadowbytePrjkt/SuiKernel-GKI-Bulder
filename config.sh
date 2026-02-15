b70776e11cd32a804ab8402cfba42af4b4618f96
ShadowbytePrjkt <chesstype438@gmail.com>
2026-02-15 10:17:52 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
 # Kernel name
-KERNEL_NAME="SuiKernel"
+KERNEL_NAME="GrayRavens-Redemption-V5"
 # Kernel Build variables
-USER="KanagawaYamada"
-HOST="HoshimachiSuisei"
+USER="ShadowbyePrjkt"
+HOST="KuchibaChisa"
 TIMEZONE="Asia/Jakarta"
 # AnyKernel
-ANYKERNEL_REPO="https://github.com/LoggingNewMemory/SuiKernel-anykernel"
+ANYKERNEL_REPO="https://github.com/ShadowbytePrjkt/GKI-anykernel"
 ANYKERNEL_BRANCH="gki"
 # Kernel Source
-KERNEL_REPO="https://github.com/LoggingNewMemory/SuiKernel-android12-5.10"
-KERNEL_BRANCH="suikernel-main"
+KERNEL_REPO="https://github.com/ramabondanp/android_kernel_common-5.10"
+KERNEL_BRANCH="android12-5.10-staging"
 KERNEL_DEFCONFIG="gki_defconfig"
 # Release repository
-GKI_RELEASES_REPO="https://github.com/LoggingNewMemory/SuiKernel-Release"
+GKI_RELEASES_REPO="https://github.com/ShadowbytePrjkt/GrayRavens-X-ShadowbytePrjkt-GKI/releases"
 # Clang
 CLANG_URL="https://github.com/LineageOS/android_prebuilts_clang_kernel_linux-x86_clang-r416183b"
 CLANG_BRANCH="lineage-20.0"
